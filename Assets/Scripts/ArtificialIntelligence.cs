49fa7e50e0bf94287577d4ff96a7f7aab92ddf16
CarlosEscobarBlasco <carlosescobarblasco@gmail.com>
2015-10-13 17:04:04 +0100

    estrellas pequeñas 2 naves inteligencia artificial a medias

---
+++ Assets/Scripts/ArtificialIntelligence.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArtificialIntelligence : MonoBehaviour {
+
+    ShipMovement movement;
+    bool rightMovement;
+
+	// Use this for initialization
+	void Start () {
+        movement = GetComponent<ShipMovement>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        RaycastHit2D hitCenter = Physics2D.Raycast(new Vector2(transform.position.x,transform.position.y + 1), Vector2.up, 3,1);
+        RaycastHit2D hitLeft = Physics2D.Raycast(new Vector2(transform.position.x - 0.6f, transform.position.y + 1), Vector2.up, 3,1);
+        RaycastHit2D hitRight = Physics2D.Raycast(new Vector2(transform.position.x + 0.6f, transform.position.y + 1), Vector2.up, 3,1);
+	    if (hitCenter.collider != null && hitCenter.collider.tag == "Obstacle")
+	    {
+	        
+	    }
+        else if (hitLeft.collider != null && hitLeft.collider.tag == "Obstacle")
+        {
+            //Debug.Log(hitLeft.collider.name + " en la izq");
+            movement.lateralMovement(true);
+        }else if (hitRight.collider != null && hitRight.collider.tag == "Obstacle")
+        {
+            //Debug.Log(hitLeft.collider.name + " en la der");
+            movement.lateralMovement(false);
+        }
+
+
+        else if (hitCenter.collider != null && hitCenter.collider.tag == "Obstacle")
+        {
+            //Debug.Log(hitLeft.collider.name + " en el centro");
+            movement.lateralMovement(rightMovement);
+        }
+        else
+        {
+            rightMovement = goCenter();
+            movement.noInput();
+        }
+        /*
+        Debug.DrawLine(new Vector3(transform.position.x, transform.position.y + 1, transform.position.z), new Vector3(transform.position.x, transform.position.y + 4, transform.position.z));
+        Debug.DrawLine(new Vector3(transform.position.x-0.6f, transform.position.y + 1, transform.position.z), new Vector3(transform.position.x-0.6f, transform.position.y + 4, transform.position.z));
+        Debug.DrawLine(new Vector3(transform.position.x+0.6f, transform.position.y + 1, transform.position.z), new Vector3(transform.position.x+0.6f, transform.position.y + 4, transform.position.z));
+        */
+	}
+
+    bool goCenter()
+    {
+        if (transform.position.x < 0)
+        {
+            return true;
+        }
+        else if (transform.position.x > 0)
+        {
+            return false;
+        }
+        else return true;
+    }
+}
---
+++ Assets/Scripts/Ship/TouchListener.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchListener : MonoBehaviour
+{
+    private ShipMovement movement;
+    // Use this for initialization
+    void Start ()
+    {
+        movement = gameObject.GetComponent<ShipMovement>();
+    }
+
+    // Update is called once per frame
+	void Update () {
+	    if (ButtonController.getInput() == "Right")
+	    {
+	        movement.lateralMovement(true);
+	    }else if (ButtonController.getInput() == "Left")
+	    {
+            movement.lateralMovement(false);
+        }
+	    else
+	    {
+            movement.noInput();
+        }
+	}
+
+}
