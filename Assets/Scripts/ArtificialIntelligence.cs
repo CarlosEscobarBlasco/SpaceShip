43e4e2913b9bd02a3195f62e9a8d1fd0f0d3f0b6
CarlosEscobarBlasco <carlosescobarblasco@gmail.com>
2015-10-13 18:40:41 +0100

    Inteligencia artificial mejorada (?)

--- Assets/Scripts/ArtificialIntelligence.cs
+++ Assets/Scripts/ArtificialIntelligence.cs
@@ -5,39 +5,53 @@ public class ArtificialIntelligence : MonoBehaviour {
 
     ShipMovement movement;
     bool rightMovement;
+    private RaycastHit2D hitCenter;
+    private RaycastHit2D hitLeft;
+    private RaycastHit2D hitRight;
+    private bool startMoving;
+    private bool myDirection;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         movement = GetComponent<ShipMovement>();
-	}
+        startMoving = true;
+    }
 	
 	// Update is called once per frame
-	void FixedUpdate () {
-        RaycastHit2D hitCenter = Physics2D.Raycast(new Vector2(transform.position.x,transform.position.y + 1), Vector2.up, 3,1);
-        RaycastHit2D hitLeft = Physics2D.Raycast(new Vector2(transform.position.x - 0.6f, transform.position.y + 1), Vector2.up, 3,1);
-        RaycastHit2D hitRight = Physics2D.Raycast(new Vector2(transform.position.x + 0.6f, transform.position.y + 1), Vector2.up, 3,1);
-	    if (hitCenter.collider != null && hitCenter.collider.tag == "Obstacle")
-	    {
-	        
-	    }
-        else if (hitLeft.collider != null && hitLeft.collider.tag == "Obstacle")
+	void FixedUpdate ()
+    {
+        hitCenter = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y + 1), Vector2.up, 3, 1);
+        hitLeft = Physics2D.Raycast(new Vector2(transform.position.x - 0.6f, transform.position.y + 1), Vector2.up, 3, 1);
+        hitRight = Physics2D.Raycast(new Vector2(transform.position.x + 0.6f, transform.position.y + 1), Vector2.up, 3, 1);
+        if (centerCollision())
         {
-            //Debug.Log(hitLeft.collider.name + " en la izq");
-            movement.lateralMovement(true);
-        }else if (hitRight.collider != null && hitRight.collider.tag == "Obstacle")
-        {
-            //Debug.Log(hitLeft.collider.name + " en la der");
-            movement.lateralMovement(false);
+            if (leftCollision() && !rightCollision())
+            {
+                betterSideToMove(startMoving || myDirection);
+                movement.lateralMovement(myDirection);
+            } else if (!leftCollision() && rightCollision())
+            {
+                betterSideToMove(!startMoving && myDirection);
+                movement.lateralMovement(myDirection);
+            }
+            else //solo el centro o los 3 a la vez
+            {
+                betterSideToMove(startMoving?rightMovement:myDirection);
+                movement.lateralMovement(myDirection);
+            }
         }
-
-
-        else if (hitCenter.collider != null && hitCenter.collider.tag == "Obstacle")
+        else if (leftCollision())
         {
-            //Debug.Log(hitLeft.collider.name + " en el centro");
-            movement.lateralMovement(rightMovement);
+            betterSideToMove(startMoving || myDirection);
+            movement.lateralMovement(myDirection);
         }
-        else
+        else if (rightCollision())
         {
+            betterSideToMove(!startMoving && myDirection);
+            movement.lateralMovement(myDirection);
+        }else
+        {
+            startMoving = true;
             rightMovement = goCenter();
             movement.noInput();
         }
@@ -46,7 +60,35 @@ public class ArtificialIntelligence : MonoBehaviour {
         Debug.DrawLine(new Vector3(transform.position.x-0.6f, transform.position.y + 1, transform.position.z), new Vector3(transform.position.x-0.6f, transform.position.y + 4, transform.position.z));
         Debug.DrawLine(new Vector3(transform.position.x+0.6f, transform.position.y + 1, transform.position.z), new Vector3(transform.position.x+0.6f, transform.position.y + 4, transform.position.z));
         */
-	}
+    }
+
+    private  void betterSideToMove(bool right)
+    {
+        myDirection = right;
+        if (startMoving)
+        {
+            startMoving = false;
+            if (right && transform.position.x > 2.3f) myDirection= false;
+            if (!right && transform.position.x < -2.3f) myDirection = true;
+        }
+    }
+
+    private bool centerCollision()
+    {
+        return hitCenter.collider != null && hitCenter.collider.tag == "Obstacle";
+    }
+
+    private bool leftCollision()
+    {
+        return hitLeft.collider != null && hitLeft.collider.tag == "Obstacle";
+    }
+
+    private bool rightCollision()
+    {
+        return hitRight.collider != null && hitRight.collider.tag == "Obstacle";
+    }
+
+
 
     bool goCenter()
     {
