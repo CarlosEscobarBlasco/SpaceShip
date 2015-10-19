4b9e649d540dcf72ea216d657389c40e3933d64a
Javier Gonzalez <javichu.12@gmail.com>
2015-10-19 13:27:41 +0100

    -Añadido un Main Panel donde iran las primeras opciones -Añadido un Scroll Panel donde se selecciona la nave deseada -Añadido los scripts asociados

---
+++ Assets/MenuController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+
+    public GameObject mainPanel;
+    public GameObject shipsPanel;
+
+	// Use this for initialization
+	void Start () {
+        shipsPanel.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void quickRace()
+    {
+        shipsPanel.SetActive(true);
+        mainPanel.SetActive(false);
+    }
+}
