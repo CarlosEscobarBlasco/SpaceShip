00653219895300950f09a5b8586a1d783dcbdd75
Javier Gonzalez <javichu.12@gmail.com>
2015-10-19 13:41:01 +0100

    -Prueba de botones y seleccion de naves

--- Assets/MenuController.cs
+++ Assets/MenuController.cs
@@ -6,10 +6,12 @@ public class MenuController : MonoBehaviour {
 
     public GameObject mainPanel;
     public GameObject shipsPanel;
+    private string selectedShip;
 
 	// Use this for initialization
 	void Start () {
         shipsPanel.SetActive(false);
+        DontDestroyOnLoad(transform.gameObject);
 	}
 	
 	// Update is called once per frame
@@ -21,5 +23,17 @@ public class MenuController : MonoBehaviour {
     {
         shipsPanel.SetActive(true);
         mainPanel.SetActive(false);
+    }
+
+    private void startQuickRace()
+    {
+        Application.LoadLevel("Game");
+        Debug.Log(selectedShip);
+    }
+
+    public void selectShip(GameObject ship)
+    {
+        selectedShip = ship.name;
+        startQuickRace();
     }
 }
