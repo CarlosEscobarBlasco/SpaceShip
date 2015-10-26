bba24057ba81cfb154bbd462f64e9cc191481e21
Javier Gonzalez <javichu.12@gmail.com>
2015-10-26 11:05:44 +0000

    -Sliders ya pillan automatico los valores

---
+++ Assets/MenuSlidersController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuSlidersController : MonoBehaviour {
+
+    public Slider accelerationSlider;
+    public Slider maxSpeedSlider;
+    public Slider gripSlider;
+
+	// Use this for initialization
+	void Start () {
+        accelerationSlider.maxValue = 0.015f;
+        accelerationSlider.minValue = 0.005f;
+        maxSpeedSlider.maxValue = 15;
+        maxSpeedSlider.minValue = 5;
+        gripSlider.maxValue = 3;
+        gripSlider.minValue = 1;
+        refreshValues();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    public void refreshValues()
+    {
+        accelerationSlider.value = (Resources.Load("Prefabs/Ships/Ship1") as GameObject).GetComponent<ShipData>().getAcceleration();
+        maxSpeedSlider.value = (Resources.Load("Prefabs/Ships/Ship1") as GameObject).GetComponent<ShipData>().getMaxSpeed();
+        gripSlider.value = (Resources.Load("Prefabs/Ships/Ship1") as GameObject).GetComponent<ShipData>().getLateralSpeeed();
+    }
+}
--- Assets/ScrollSnapRect.cs
+++ Assets/ScrollSnapRect.cs
@@ -59,6 +59,7 @@ public class ScrollSnapRect : MonoBehaviour, IBeginDragHandler, IEndDragHandler,
     // container with Image components - one Image for each page
     private List<Image> _pageSelectionImages;
 
+    public MenuSlidersController sliderController;
     //------------------------------------------------------------------------
     void Start() {
         _scrollRectComponent = GetComponent<ScrollRect>();
@@ -176,6 +177,7 @@ public class ScrollSnapRect : MonoBehaviour, IBeginDragHandler, IEndDragHandler,
         _lerpTo = _pagePositions[aPageIndex];
         _lerp = true;
         _currentPage = aPageIndex;
+        sliderController.refreshValues();
     }
 
     //------------------------------------------------------------------------
