00402e63a51b4e64cada5ebecc8c866988c7f90e
Javier Gonzalez <javichu.12@gmail.com>
2016-01-29 15:29:07 +0000

    -Comentados los power ups -Comienzo de la pausa

---
+++ Assets/PauseButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PauseButton : MonoBehaviour
+{
+
+    public GameObject pausePanel;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void pauseGame()
+    {
+        pausePanel.SetActive(true);
+        Time.timeScale = 0.0f;
+    }
+
+
+}
